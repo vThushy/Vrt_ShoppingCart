af3d1ab22ba1da3bf7a61ff64fed4d503e241919
vThushy <vthushy11@gmail.com>
2020-06-28 20:42:11 +0530

    Client: Initial commit after modules create

--- Project/ShoppingCartClient/src/app/app.module.ts
+++ Project/ShoppingCartClient/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { SliderComponent } from './slider/slider.component';
+import { CategoryComponent } from './category/category.component';
+import { SellerComponent } from './seller/seller.component';
+import { SocialComponent } from './social/social.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    SliderComponent,
+    CategoryComponent,
+    SellerComponent,
+    SocialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
