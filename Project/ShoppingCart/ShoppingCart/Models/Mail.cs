013674229d0e0a1def1f203b4b407e464a5999c8
vThushy <vthushy11@gmail.com>
2020-07-30 21:55:50 +0530

    Implementing smtpclient to send a mail

---
+++ Project/ShoppingCart/ShoppingCart/Models/Mail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Mail
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+    }
+}
+}
---
+++ Project/ShoppingCart/ShoppingCart/Utility/EMail.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using ShoppingCart.Models;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Mail;
+
+namespace ShoppingCart.Utility
+{
+    public class EMail
+    {
+
+        private IConfiguration _configuration;
+
+        public EMail(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SmtpClient GetSmtp()
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = _configuration.GetValue<string>("MailConfiguration:Host");
+                smtp.Port = _configuration.GetValue<int>("MailConfiguration:Port"); 
+                smtp.EnableSsl = _configuration.GetValue<bool>("MailConfiguration:SSL"); 
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(_configuration.GetValue<string>("MailConfiguration:UserName"),
+                    _configuration.GetValue<string>("MailConfiguration:Password"));
+                return smtp;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Send(Mail mail)
+        {
+            try
+            {
+                if(mail != null)
+                {
+                    using (var message = new MailMessage(mail.ToEmail, mail.ToEmail))
+                    {
+                        message.Subject = mail.Subject;
+                        message.Body = mail.Body;
+                        message.IsBodyHtml = mail.IsHtml;
+                        GetSmtp().Send(message);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            
+        }
+    }
+
+    public class MailConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool SSL { get; set; }
+    }
+
+  
+
+}
