d94538670955f36be4f86640bd7fd31a264220b3
vThushy <vthushy11@gmail.com>
2020-08-19 15:46:38 +0530

    Added categories const

---
+++ Project/ShoppingCartClient/src/app/Util/categories.ts
@@ -0,0 +1,41 @@
+import { imagePath } from './paths';
+
+export const mainCategories = [
+    ['SHOP WOMENs', 'women', imagePath.category_women],
+    ['SHOP MENs', 'men', imagePath.category_men],
+    ['SHOP KIDs', 'kid', imagePath.category_kid],
+    ['SHOP LIFESTYLE', 'lifestyle', imagePath.category_lifestyle],
+    ['SHOP GIFT', 'gift', imagePath.category_gift]
+];
+
+export const womenCategories = [
+    ['CASUAL WEAR', 'women_casual', imagePath.category_women_casual],
+    ['FORMAL WEAR', 'women_formal', imagePath.category_women_formal],
+    ['SPORT WEAR', 'women_sport', imagePath.category_women_sport],
+    ['FOOT WEAR', 'women_foot', imagePath.category_women_foot],
+    ['ACCESSORIES', 'women_accessories', imagePath.category_women_accessories]
+];
+
+export const menCategories = [
+    ['CASUAL WEAR', 'men-casual', imagePath.category_men_casual],
+    ['FORMAL WEAR', 'men-formal', imagePath.category_men_formal],
+    ['SPORT WEAR', 'men_sport', imagePath.category_men_sport],
+    ['FOOT WEAR', 'men_foot', imagePath.category_men_foot],
+    ['ACCESSORIES', 'men_accessories', imagePath.category_men_accessories]
+];
+
+export const kidCategories = [
+    ['GIRLS', 'kid_girl', imagePath.category_kid_girl],
+    ['BOYS', 'kid_boy', imagePath.category_kid_boy],
+    ['FOOT WEAR', 'kid_foot', imagePath.category_kid_foot],
+    ['TOYS', 'kid_toy', imagePath.category_kid_toy],
+    ['ACCESSORIES', 'kid_accessories', imagePath.category_kid_accessories],
+];
+
+export const lifeStyleCategories = [
+    ['HOME WEARS', 'home-wear', imagePath.category_lifestyle_home],
+    ['KITCHEN WEARS', 'kitchen-wear', imagePath.category_lifestyle_kitchen],
+    ['BATH WEARS', 'bath-wear', imagePath.category_lifestyle_bath],
+    ['WALL DECORS', 'wall-decor', imagePath.category_lifestyle_wall],
+    ['HOME GARDEN', 'home-garden', imagePath.category_lifestyle_garden],
+];
