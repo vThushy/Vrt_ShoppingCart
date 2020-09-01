1ff1c36c9a4e427315cbc46747e3d04578d94fbd
vThushy <vthushy11@gmail.com>
2020-09-01 14:47:22 +0530

    Add size, qty in Product model impl parameterized constructor when create obj

--- Project/ShoppingCartClient/src/app/Models/Product.ts
+++ Project/ShoppingCartClient/src/app/Models/Product.ts
@@ -6,5 +6,20 @@ export class Product{
     discount: number;
     price: number;
     defaultImage: string;
-    qty: number = 1;
+    size: string;
+    qty: number
+
+
+    constructor(id: number, categoryId: number, name: string, description: string, 
+        discount: number,  price: number,  defaultImage: string,  size: string, qty: number = 1){
+            this.id = id;
+            this.categoryId = categoryId;
+            this.name = name;
+            this.description = description;
+            this.discount = discount;
+            this.price = price;
+            this.defaultImage = defaultImage;
+            this.size = size;
+            this.qty = qty;
+    }
 }
