73532ad0a0bd99aba879629d7f3c93799a4f6853
vThushy <vthushy11@gmail.com>
2020-09-01 14:33:34 +0530

    Add Different types and sizes to size.enum

--- Project/ShoppingCart/ShoppingCart/Enum/Sizes.cs
+++ Project/ShoppingCart/ShoppingCart/Enum/Sizes.cs
@@ -7,11 +7,34 @@ namespace ShoppingCart.Enum
 {
     public enum ProductSize
     {
+        // Dress-sizes
         XS = 0,
         S = 1,
         M = 2,
         L = 3,
         XL = 4,
-        XXL = 5
+        XXL = 5,
+
+        //Furniture-sizes
+        SMALL = 20,
+        MEDIUM = 21,
+        LARGE = 22,
+
+
+        // Shoe-sizes
+        S36 = 36,
+        S37 = 37,
+        S38 = 38,
+        S39 = 39,
+        S40 = 40,
+        S41 = 41,
+        S42 = 42,
+        S43 = 43,
+        S44 = 44,
+
+
+        
+
+
     }
 }
