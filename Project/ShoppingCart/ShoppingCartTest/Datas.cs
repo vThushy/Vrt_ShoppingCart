d50fc01a4ab5a61a20715ef8d182089e62369d2c
vThushy <vthushy11@gmail.com>
2020-09-07 21:36:53 +0530

    Create class to centralise the data for use in different test cases

---
+++ Project/ShoppingCart/ShoppingCartTest/Datas.cs
@@ -0,0 +1,62 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCartTest
+{
+    public class Datas
+    {
+        public Address SetAddressForTest
+        {
+            get
+            {
+                Address address = new Address()
+                {
+                    Id = 1,
+                    UserName = "vThushy",
+                    AddressType = "Home",
+                    AddressLine = "Kachchceri Road",
+                    City = "Jaffna",
+                    State = "Northern",
+                    Country = "Sri Lanka",
+                    ZipCode = "40000"
+                };
+                return address;
+            }
+        }
+
+        public List<Address> SetListOfAddressForTest
+        {
+            get
+            {
+                var addresses = new List<Address>();
+                addresses.Add(new Address()
+                {
+                    Id = 1,
+                    UserName = "vThushy",
+                    AddressType = "Home",
+                    AddressLine = "Kachchceri Road",
+                    City = "Jaffna",
+                    State = "Northern",
+                    Country = "Sri Lanka",
+                    ZipCode = "40000"
+                });
+                addresses.Add(new Address()
+                {
+                    Id = 2,
+                    UserName = "vThushy",
+                    AddressType = "Work",
+                    AddressLine = "Danister silva mw",
+                    City = "Dematagoda",
+                    State = "Western",
+                    Country = "Sri Lanka",
+                    ZipCode = "17000"
+                });
+
+                return addresses;
+            }
+        }
+
+    }
+}
