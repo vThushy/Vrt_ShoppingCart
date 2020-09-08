772eb93956effe61077120feb51a8294e598795b
vThushy <vthushy11@gmail.com>
2020-09-08 12:05:57 +0530

    Datas for test authentication controller

--- Project/ShoppingCart/ShoppingCartTest/Datas.cs
+++ Project/ShoppingCart/ShoppingCartTest/Datas.cs
@@ -1,4 +1,6 @@
-﻿using ShoppingCart.Models;
+﻿using ShoppingCart.Controllers;
+using ShoppingCart.Enum;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +9,66 @@ namespace ShoppingCartTest
 {
     public class Datas
     {
+        public string SetUserNameForTest
+        {
+            get
+            {
+                return "vThushy";
+            }
+        }
+        public CustomerRegObj SetCustomerRegObjForTest
+        {
+            get
+            {
+                CustomerRegObj customerRegObj = new CustomerRegObj()
+                {
+                    User = SetUserForTest,
+                    Customer = SetCustomerForTest,
+                    Address = SetAddressForTest
+                };
+                return customerRegObj;
+            }
+        }
+        public ResponseUser SetResponseUserForTest
+        {
+            get
+            {
+                ResponseUser responseUser = new ResponseUser("vThushy", "DummyToken");
+                return responseUser;
+            }
+        }
+        public User SetUserForTest
+        {
+            get
+            {
+                User user = new User()
+                {
+                    UserName = "Thushy",
+                    Password = "0Thushy",
+                    UserRole = AccessRole.Admin,
+                    ResetCode = "1"
+                };
+                return user;
+            }
+
+        }
+        public Customer SetCustomerForTest
+        {
+            get
+            {
+                Customer customer = new Customer()
+                {
+                    UserName = "Thushy",
+                    FirstName = "Thushy",
+                    LastName = "Vimal",
+                    //DateOfBirth = "",
+                    Gender = Gender.Female,
+                    Email = "vthushy@gmail.com",
+                };
+                return customer;
+            }
+
+        }
         public Address SetAddressForTest
         {
             get
@@ -14,7 +76,7 @@ namespace ShoppingCartTest
                 Address address = new Address()
                 {
                     Id = 1,
-                    UserName = "vThushy",
+                    UserName = "Thushy",
                     AddressType = "Home",
                     AddressLine = "Kachchceri Road",
                     City = "Jaffna",
