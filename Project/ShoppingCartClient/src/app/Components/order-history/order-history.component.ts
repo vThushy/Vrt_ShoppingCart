7a1563f9a664340c762ea57cbda480ecb0242c28
vThushy <vthushy11@gmail.com>
2020-09-15 23:07:00 +0530

    Order-History view list implementation

--- Project/ShoppingCartClient/src/app/Components/order-history/order-history.component.ts
+++ Project/ShoppingCartClient/src/app/Components/order-history/order-history.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderWithDetails, OrderLines } from 'src/app/Models/Order';
+import { OrderService } from 'src/app/Services/order.service';
+import { UsersService } from 'src/app/Services/users.service';
+import { ExceptionHandlerService } from 'src/app/Util/exception-handler.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-history',
@@ -7,9 +12,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderHistoryComponent implements OnInit {
 
-  constructor() { }
+  orders: OrderWithDetails[];
+  activeOrders: OrderWithDetails[];
+  
+  selectedOrder: OrderWithDetails;
+
+  constructor(
+    private orderService: OrderService,
+    private userService: UsersService,
+    private exceptionHandlerService: ExceptionHandlerService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    let userName = this.userService.getUserName();
+    if (userName != null) {
+      this.orderService.getOrdersByCustomer(userName).subscribe(
+        result => {
+          this.orders = result;
+        },
+        error => {
+          this.exceptionHandlerService.handleError(error);
+        });
+      this.orderService.getActiveOrdersByCustomer(userName).subscribe(
+        result => {
+          this.activeOrders = result;
+          console.log(this.orders);
+        },
+        error => {
+          this.exceptionHandlerService.handleError(error);
+        });
+    }
   }
 
+  public getTotal(orderId: number): number {
+    let sum: number = 0;
+    this.orders.forEach(element => {
+      if (element.order.id == orderId) {
+        sum = this.calculateTotal(element);
+      }
+    });
+    return sum;
+  }
+
+  calculateTotal(order: OrderWithDetails): number {
+    let total: number = 0;
+    order.orderLines.forEach(line => {
+      total += line.unitPrice;
+    });
+    return total;
+  }
+
+
+  getOrderStatus(status: string): string {
+    console.log(status);
+    let returnStatus: string;
+    if (status == '0') {
+      returnStatus = "Active";
+    }
+    else if (status == '1') {
+      returnStatus = "Paid";
+    }
+    else if (status == '2') {
+      returnStatus = "Accepted";
+    }
+    else if (status == '3') {
+      returnStatus = "Shipped";
+    }
+    else if (status == '4') {
+      returnStatus = "Delivered";
+    }
+    else if (status == '5') {
+      returnStatus = "Completed";
+    }
+    return returnStatus;
+  }
+
+  setSelectedOrder(order: OrderWithDetails){
+    this.selectedOrder = order;
+  }
+
+  checkout(order: OrderWithDetails){
+    localStorage.removeItem('tmp-order');
+    localStorage.setItem('tmp-order', order.order.id.toString());
+    this.router.navigate(['/payment','history']);
+  }
 }
--- Project/ShoppingCartClient/src/app/Services/order.service.ts
+++ Project/ShoppingCartClient/src/app/Services/order.service.ts
@@ -1,25 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ExceptionHandlerService } from '../Util/exception-handler.service';
 import { Observable } from 'rxjs';
-import { OrderCustomerAPI } from '../Util/config';
+import { OrderCustomerAPI, OrderAPI, ActiveOrderCustomerAPI } from '../Util/config';
 import { catchError } from 'rxjs/operators';
 import { OrderWithDetails } from '../Models/Order';
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  token: string;
 
   constructor(
     private httpClient: HttpClient,
-    private exceptionHandlerService: ExceptionHandlerService
-  ) { }
+    private exceptionHandlerService: ExceptionHandlerService,
+    private userService: UsersService
+  ) {
+    this.token = userService.getAuthToken();
+  }
 
   getOrdersByCustomer(userName: string): Observable<OrderWithDetails[]> {
     return this.httpClient.get<OrderWithDetails[]>(OrderCustomerAPI + '/' + userName)
       .pipe(
         catchError(this.exceptionHandlerService.handleError)
       );
+  }
+
+  getActiveOrdersByCustomer(userName: string): Observable<OrderWithDetails[]> {
+    return this.httpClient.get<OrderWithDetails[]>(ActiveOrderCustomerAPI + '/' + userName)
+      .pipe(
+        catchError(this.exceptionHandlerService.handleError)
+      );
+  }
+
+  getOrder(orderId: string): Observable<OrderWithDetails> {
+    return this.httpClient.get<OrderWithDetails>(OrderAPI + '/' + orderId)
+      .pipe(
+        catchError(this.exceptionHandlerService.handleError)
+      );
+  }
+
+  checkout(order: OrderWithDetails) {
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.token
+    })
+    return this.httpClient.post<any>(OrderAPI, JSON.stringify(order), { headers: header })
+      .pipe(catchError(this.exceptionHandlerService.handleError));
   }
 }
